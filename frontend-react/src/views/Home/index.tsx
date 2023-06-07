bdee130bd60bc92fcd8da9bd7fbb6312c07d8abd
Gabriele Dal Cengio <dalcengio.gabriele@gmail.com>
2023-06-07 15:16:09 -0700

    Landing view leads to map view in one component for hacky simplicity

--- frontend-react/src/views/Home/index.tsx
+++ frontend-react/src/views/Home/index.tsx
@@ -14,8 +14,68 @@ import {
 import BCHeader from '../../components/BCHeader';
 
 const Home: React.FC = () => {
-  const [currPosition, setCurrPosition] = useState([53, -127] as LatLngExpression)<LatLngExpression>;
+  // const [currPosition, setCurrPosition] = useState([53, -127] as LatLngExpression)<LatLngExpression>;
   const [displayData, setDisplayData] = useState(campgrounds);
+  const [formToggle, setFormToggle] = useState(true);
+
+  const Landing: React.FC = () => {
+    return (
+      <>
+        <FlexGrid className="mainContainer">
+          <form>
+          <label>
+            Select A Park:
+            <select name="park">
+              <option value="all-parks">All Parks</option>
+              <option value="banana">Banana</option>
+              <option value="orange">Orange</option>
+            </select>
+          </label>
+          <label>
+            Park Region:
+            <select name="region">
+              <option value="southern-interior">Southern Interior</option>
+              <option value="banana">Banana</option>
+              <option value="orange">Orange</option>
+            </select>
+          </label>
+          <label>
+            Date From:
+            <input type="date" name="name" />
+          </label>
+          <label>
+            Date To:
+            <input type="date" name="name" />
+          </label>
+          <label>
+            Nights:
+            <input type="number" name="name" />
+          </label>
+          <label>
+            Equipment:
+            <input type="text" name="name" />
+          </label>
+          <label>
+            Party:
+            <input type="text" name="name" />
+          </label>
+            <Button onClick={() => setFormToggle(false)}>Test</Button>
+          </form>
+        </FlexGrid>
+      </>
+    );
+  };
+
+  const Toggles: React.FC = () => {
+    return (
+      <>
+        <FlexGrid className="toggles">
+          <Button>Map</Button>
+          <Button>Map</Button>
+        </FlexGrid>
+      </>
+    );
+  };
 
   return (
     <>
@@ -23,31 +83,35 @@ const Home: React.FC = () => {
       <FlexGrid className="mainContainer">
         <div>
           <h1 data-testid="home-title">BC Campgrounds</h1> 
-          <MapContainer center={[53, -127]} zoom={6} scrollWheelZoom={false} className="map">
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={currPosition}>
-              <Popup>
-                Current position
-              </Popup>
-            </Marker>
-
-            {
-              displayData.map((site, index) => (
-                <Marker key={index} position={{lat: site?.geo?.latitude, lng: site?.geo?.longitude}}>
-                  <Popup>
-                    <h4>
-                      {site?.name}
-                    </h4>
-                    {site?.description}
-                  </Popup>
-                </Marker>
-              ))
+          {
+            formToggle && <Landing></Landing>
+          }
+          {
+            !formToggle 
+            &&
+            <FlexGrid className="mapContainer">
+              <Toggles></Toggles>
+              <MapContainer center={[53, -127]} zoom={6} scrollWheelZoom={true} className="map">
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {
+                  displayData.map((site, index) => (
+                    <Marker key={index} position={{lat: site?.geo?.latitude, lng: site?.geo?.longitude}}>
+                      <Popup>
+                        <h4>
+                          {site?.name}
+                        </h4>
+                        {site?.description}
+                      </Popup>
+                    </Marker>
+                  ))
+                }
+              </MapContainer> 
+            </FlexGrid>
             }
-        </MapContainer>
-        </div>
+          </div>
       </FlexGrid>
     </>
   );
--- frontend-react/src/views/Landing/index.tsx
+++ frontend-react/src/views/Landing/index.tsx
@@ -1,89 +1,27 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { KeycloakLoginOptions } from 'keycloak-js';
+import React, { useState } from 'react';
 
 import {
+  Tile,
   Button,
   FlexGrid,
   Column,
   Row,
   Stack
 } from '@carbon/react';
-import LoginProviders from '../../types/LoginProviders';
-import { useAuth } from '../../contexts/AuthContext';
+import BCHeader from '../../components/BCHeader';
+import { useNavigate } from 'react-router-dom';
 
-const Landing = () => {
-  const { startKeycloak, login, signed } = useAuth();
+const Landing: React.FC = () => {
   const navigate = useNavigate();
-
-  const getPageParam = (): string => {
-    let page: string = '/home';
-    const paramString = window.location.search.split('?')[1];
-    const queryString = new URLSearchParams(paramString);
-    if (queryString.has('page')) {
-      page = queryString.get('page') || '/home';
-    }
-    return page;
-  };
-
-  const handleLogin = (provider: LoginProviders) => {
-    if (signed) {
-      navigate(getPageParam());
-      return;
-    }
-
-    const loginOptions: KeycloakLoginOptions = {
-      redirectUri: `${window.location.origin}/home`,
-      idpHint: provider
-    };
-
-    login(loginOptions);
-  };
-
-  useEffect(() => {
-    if (signed) {
-      navigate(getPageParam());
-    } else {
-      startKeycloak();
-    }
-  }, [signed]);
-
   return (
-    <FlexGrid className="mainContainer">
-      <FlexGrid container="true" spacing={4}>
-        <Stack gap={6}>
-          <Row>
-            <Column sm={4}>
-              <Stack gap={3}>
-                <h1 data-testid="home-title">NR React Sample App</h1>
-                <p data-testid="home-desc">
-                  Landing page
-                </p>
-              </Stack>
-            </Column>
-          </Row>
-          <Row>
-            <Column sm={4} md={4}>
-              <Button
-                onClick={() => { handleLogin(LoginProviders.IDIR); }}
-                size="lg"
-                data-testid="landing-button__idir"
-              >
-                Login with IDIR
-              </Button>
-              &nbsp;
-              <Button
-                onClick={() => { handleLogin(LoginProviders.BUSINESS_BCEID); }}
-                size="lg"
-                data-testid="landing-button__bceid"
-              >
-                Login with Business BCeID
-              </Button>
-            </Column>
-          </Row>
-        </Stack>
+    <>
+      <BCHeader />
+      <FlexGrid className="mainContainer">
+        <form>
+          <input type="submit" value="submit" onSubmit={() => navigate('/home')}></input>
+        </form>
       </FlexGrid>
-    </FlexGrid>
+    </>
   );
 };
 
